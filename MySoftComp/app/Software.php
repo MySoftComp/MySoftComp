62bf7670f6dd22e4a6d677e159c02fb2ec84b083
nugrohofigo <43537280+nugrohofigo@users.noreply.github.com>
2019-01-10 21:14:50 +0700

    Add files via upload

---
+++ MySoftComp/app/Software.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class software extends Model
+{
+    //
+    protected $table = 'software';
+    public function category(){
+        return $this->belongsTo(category::class,'category_id');
+    }
+    public $timestamps = false;
+}
